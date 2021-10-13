b73c6f20ffee828d9ea15b37ce0a09d85833b1a1
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-10-13 12:27:21 +0100

    Exporet service API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './index/index.js'
 export * from './messages/index.js'
+export * from './service/index.js'
 export * from './step-documents/index.js'
 export * from './tree-sitter/index.js'
---
+++ src/service/index.ts
@@ -0,0 +1,4 @@
+export * from './getGherkinCompletionItems'
+export * from './getGherkinDiagnostics.js'
+export * from './getGherkinFormattingEdits'
+export * from './getGherkinSemanticTokens'
