ffd61d78d479e72482cd320c82cda182302d5e77
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-10-21 21:41:50 +0100

    linting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export * from './gherkin/index.js'
 export * from './index/index.js'
 export * from './messages/index.js'
 export * from './service/index.js'
 export * from './step-documents/index.js'
 export * from './tree-sitter/index.js'
-export * from './gherkin/index.js'
