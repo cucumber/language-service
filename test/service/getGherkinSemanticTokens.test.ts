2d5c6eace5bdf69928d12cc096a36d43809dd6fb
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-11-08 18:57:23 +0000

    Improve legibility of token assertions

--- test/service/getGherkinSemanticTokens.test.ts
+++ test/service/getGherkinSemanticTokens.test.ts
@@ -1,8 +1,13 @@
 import { CucumberExpression, ParameterTypeRegistry } from '@cucumber/cucumber-expressions'
 import assert from 'assert'
-import { SemanticTokens } from 'vscode-languageserver-types'
+import { SemanticTokenTypes, uinteger } from 'vscode-languageserver-types'
 
-import { getGherkinSemanticTokens } from '../../src/service/getGherkinSemanticTokens.js'
+import {
+  getGherkinSemanticTokens,
+  semanticTokenTypes,
+} from '../../src/service/getGherkinSemanticTokens.js'
+
+type TokenWithType = [string, SemanticTokenTypes]
 
 describe('getGherkinSemanticTokens', () => {
   it('creates tokens for keywords', () => {
@@ -35,144 +40,58 @@ Feature: a
     )
 
     const semanticTokens = getGherkinSemanticTokens(gherkinSource, [expression])
-    // TODO: Rather than expecting a long line of ints here, parse it (as vscode would), and derive a list of "highlighted tokens"
-    // like [["@foo", "type"], ["@bar", "type"], ["Feature", "keyword"], ...]
-    const expectedSemanticTokens: SemanticTokens = {
-      // See https://microsoft.github.io/language-server-protocol/specifications/specification-3-17/#textDocument_semanticTokens
-      // for details about how tokens are encoded
-      data: [
-        1,
-        0,
-        4,
-        3,
-        0, // @foo
-        0,
-        5,
-        4,
-        3,
-        0, // @bar
-        1,
-        0,
-        7,
-        0,
-        0, // Feature
-        4,
-        2,
-        8,
-        0,
-        0, // Scenario
-        1,
-        4,
-        6,
-        0,
-        0, // Given
-        0,
-        13,
-        2,
-        1,
-        0, // 42
-        0,
-        15,
-        5,
-        1,
-        0, // belly
-        1,
-        6,
-        3,
-        2,
-        0, // """
-        0,
-        3,
-        8,
-        3,
-        0, // sometype
-        1,
-        5,
-        5,
-        2,
-        0, // hello
-        1,
-        8,
-        5,
-        2,
-        0, // world
-        1,
-        7,
-        3,
-        2,
-        0, // """
-        1,
-        4,
-        4,
-        0,
-        0, // And
-        1,
-        8,
-        1,
-        2,
-        0, // a
-        0,
-        5,
-        3,
-        2,
-        0, // bbb
-        1,
-        8,
-        2,
-        2,
-        0, // cc
-        0,
-        6,
-        2,
-        2,
-        0, // dd
-        2,
-        2,
-        16,
-        0,
-        0, // Scenario Outline
-        1,
-        4,
-        6,
-        0,
-        0, // Given
-        0,
-        8,
-        5,
-        4,
-        0, // <foo>
-        0,
-        10,
-        5,
-        4,
-        0, // <bar>
-        2,
-        4,
-        8,
-        0,
-        0, // Examples
-        1,
-        8,
-        3,
-        5,
-        0, // foo
-        0,
-        6,
-        3,
-        5,
-        0, // bar
-        1,
-        8,
-        1,
-        2,
-        0, // a
-        0,
-        6,
-        1,
-        2,
-        0, // b
-      ],
-    }
-    assert.deepStrictEqual(semanticTokens, expectedSemanticTokens)
+    const actual = tokenize(gherkinSource, semanticTokens.data)
+    const expected: TokenWithType[] = [
+      ['@foo', SemanticTokenTypes.type],
+      ['@bar', SemanticTokenTypes.type],
+      ['Feature', SemanticTokenTypes.keyword],
+      ['Scenario', SemanticTokenTypes.keyword],
+      ['Given ', SemanticTokenTypes.keyword],
+      ['42', SemanticTokenTypes.parameter],
+      ['belly', SemanticTokenTypes.parameter],
+      ['"""', SemanticTokenTypes.string],
+      ['sometype', SemanticTokenTypes.type],
+      ['hello', SemanticTokenTypes.string],
+      ['world', SemanticTokenTypes.string],
+      ['"""', SemanticTokenTypes.string],
+      ['And ', SemanticTokenTypes.keyword],
+      ['a', SemanticTokenTypes.string],
+      ['bbb', SemanticTokenTypes.string],
+      ['cc', SemanticTokenTypes.string],
+      ['dd', SemanticTokenTypes.string],
+      ['Scenario Outline', SemanticTokenTypes.keyword],
+      ['Given ', SemanticTokenTypes.keyword],
+      ['<foo>', SemanticTokenTypes.variable],
+      ['<bar>', SemanticTokenTypes.variable],
+      ['Examples', SemanticTokenTypes.keyword],
+      ['foo', SemanticTokenTypes.property],
+      ['bar', SemanticTokenTypes.property],
+      ['a', SemanticTokenTypes.string],
+      ['b', SemanticTokenTypes.string],
+    ]
+    assert.deepStrictEqual(actual, expected)
   })
 })
+
+// See https://microsoft.github.io/language-server-protocol/specifications/specification-3-17/#textDocument_semanticTokens
+// for details about how tokens are encoded
+function tokenize(source: string, tokenData: readonly uinteger[]): readonly TokenWithType[] {
+  const result: TokenWithType[] = []
+  const lines = source.split('\n')
+  let lineIndex = 0
+  let start = 0
+  for (let i = 0; i < tokenData.length; i += 5) {
+    const deltaLine = tokenData[i]
+    if (deltaLine > 0) {
+      start = 0
+    }
+    lineIndex += deltaLine
+    start += tokenData[i + 1]
+    const length = tokenData[i + 2]
+    const token = lines[lineIndex].substring(start, start + length)
+    const tokenTypeIndex = tokenData[i + 3]
+    const tokenType = semanticTokenTypes[tokenTypeIndex]
+    result.push([token, tokenType])
+  }
+  return result
+}
