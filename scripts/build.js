0ff83d76b6beb2baf819d1032acb48be3812f051
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-04-25 10:30:19 +0100

    Try to fix build

--- scripts/build.js
+++ scripts/build.js
@@ -4,8 +4,7 @@ import { exec } from 'child_process'
 import fs from 'fs'
 import path from 'path'
 
-// Language-package map
-const langMap = {
+const languages = {
   typescript: ['typescript', 'typescript'],
   java: ['java'],
 }
@@ -15,14 +14,18 @@ const parsersDir = 'dist'
 if (!fs.existsSync(parsersDir)) {
   fs.mkdirSync(parsersDir)
 }
-for (const [lang, names] of Object.entries(langMap)) {
+for (const [lang, names] of Object.entries(languages)) {
   const [moduleName, ...variant] = names
   const module = path.join('node_modules/tree-sitter-' + moduleName, ...variant)
   const output = 'tree-sitter-' + names[names.length - 1] + '.wasm'
 
   console.log('Compiling ' + lang + ' parser')
   // https://github.com/tree-sitter/tree-sitter/issues/1560
-  exec(`node_modules/.bin/tree-sitter build-wasm ${module} --docker`, (err) => {
+  let command = `node_modules/.bin/tree-sitter build-wasm ${module}`
+  if (!process.env.CI) {
+    command += ' --docker'
+  }
+  exec(command, (err) => {
     if (err) {
       console.error('Failed to build wasm for ' + lang + ': ' + err.message)
       process.exit(1)
