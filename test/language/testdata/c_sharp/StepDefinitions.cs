9477487d821804e2f822a255754e8c47677230c4
Adam Figgins <atfiggins@gmail.com>
2022-06-03 15:00:10 +0100

    fix(c-sharp): Fixing csharpLanguage to support cucumberexpression syntax
    - Also updating to support non-verbatim string literal expressions

--- test/language/testdata/c_sharp/StepDefinitions.cs
+++ test/language/testdata/c_sharp/StepDefinitions.cs
@@ -5,8 +5,24 @@ namespace StepDefinitions
     [Binding]
     public class BowlingSteps
     {
-        [Given(@"^a regexp$")]
-        public void a_regexp() {
+		[Given(@"^a regexp$")]
+        public void ARegexp() 
+		{
         }
+
+		[When(@"I have Regex parameters like (\d+) and (\d+)")]
+		public void WhenIHaveRegexParametersLikeAnd(int pins1, int pins2)
+		{
+		}
+
+		[Then("a {string} cucumber expression {string}")]
+		public void ThenACucumberExpression(string s, string s2)
+		{
+		}
+
+		[Then(@"I have a verbatim string")]
+		public void ThenIHaveAVerbatimString()
+		{
+		}
     }
 }
