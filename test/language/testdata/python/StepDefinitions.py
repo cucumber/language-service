4892bfdf57e015802bde08c6cb9789c76446c42e
Vsevolod Popov <sevapopov13@gmail.com>
2022-08-24 14:21:14 +0300

    Add pytest-bdd support

--- test/language/testdata/python/StepDefinitions.py
+++ test/language/testdata/python/StepDefinitions.py
@@ -1,10 +1,16 @@
 """Port of givens for testdata."""
 from behave import given
+from pytest_bdd import given
 import datetime
 import re
 
 
 @given("^a regexp$")
 def step_re(expression):
+    """Test Re."""
+    assert expression
+
+@given("^a regexp$")
+def test_re(expression):
     """Test Re."""
     assert expression
