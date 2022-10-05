5a8fd95fa3e871abcd80ab8484ce465b31fe1d74
王雪 <devel0per@yeah.net>
2022-10-05 01:33:13 +0000

    Add support for rust

--- src/language/rustLanguage.ts
+++ src/language/rustLanguage.ts
@@ -3,6 +3,9 @@ import { Language, TreeSitterSyntaxNode } from './types.js'
 export const rustLanguage: Language = {
   toParameterTypeName(node) {
     switch (node.type) {
+      case 'raw_string_literal': {
+        return stringLiteral(node)
+      }
       case 'string_literal': {
         return stringLiteral(node)
       }
@@ -25,92 +28,75 @@ export const rustLanguage: Language = {
 
   defineParameterTypeQueries: [
     `
-(method_declaration 
-  (modifiers 
-    (annotation 
-      name: (identifier) @annotation-name 
-      arguments: (annotation_argument_list
+(attribute_item 
+  (meta_item 
+    (identifier) @meta-name
+    arguments: (meta_arguments
         [
-          (string_literal) @expression
+          ((meta_item
+          	(identifier) @name-key
+            value: (string_literal) @name
+          )(meta_item
+          	(identifier) @value-key
+            value: (string_literal) @expression
+          ))
+          ((meta_item
+          	(identifier) @value-key
+            value: (string_literal) @expression
+          )(meta_item
+          	(identifier) @name-key
+            value: (string_literal) @name
+          ))
         ]
-      )
     )
   )
-  name: (identifier) @name
-  (#eq? @annotation-name "ParameterType")
-) @root
-    `,
-    `
-(method_declaration 
-  (modifiers 
-    (annotation 
-      name: (identifier) @annotation-name 
-      arguments: (annotation_argument_list
-        [
-          (
-            (element_value_pair
-              key: (identifier) @name-key
-              value: (string_literal) @name
-            )
-            (element_value_pair
-              key: (identifier) @value-key
-              value: (string_literal) @expression
-            )
-          )
-          (
-            (element_value_pair
-              key: (identifier) @value-key
-              value: (string_literal) @expression
-            )
-            (element_value_pair
-              key: (identifier) @name-key
-              value: (string_literal) @name
-            )
-          )
-        ]
-      )
-    )
-  )
-  (#eq? @annotation-name "ParameterType")
+  (#eq? @meta-name "param")
   (#eq? @name-key "name")
-  (#eq? @value-key "value")
+  (#eq? @value-key "regex")
 ) @root
 `,
   ],
   defineStepDefinitionQueries: [
     `
-(method_declaration 
-  (modifiers 
-    (annotation 
-      name: (identifier) @annotation-name 
-      arguments: (annotation_argument_list
+(attribute_item 
+  (meta_item 
+    (
+    	(identifier) @meta-name 
+      arguments: (meta_arguments
         [
           (string_literal) @expression
+          (meta_item
+            value: (string_literal) @expression)
+          (meta_item
+            (identifier)
+            value: (string_literal) @expression)
+          (meta_item
+            value: (raw_string_literal) @expression)
         ]
       )
     )
-  )
-  (#match? @annotation-name "Given|When|Then")
-) @root
+  ) @root
+  (#match? @meta-name "given|when|then")
+)
 `,
   ],
 
   snippetParameters: {
-    int: { type: 'int', name: 'i' },
-    float: { type: 'float', name: 'f' },
+    int: { type: 'i32', name: 'i' },
+    float: { type: 'f32', name: 'f' },
     word: { type: 'String' },
     string: { type: 'String', name: 's' },
-    double: { type: 'double', name: 'd' },
-    bigdecimal: { type: 'java.math.BigDecimal', name: 'bigDecimal' },
-    byte: { type: 'byte', name: 'b' },
-    short: { type: 'short', name: 's' },
-    long: { type: 'long', name: 'l' },
-    biginteger: { type: 'java.math.BigInteger', name: 'bigInteger' },
-    '': { type: 'Object', name: 'arg' },
+    double: { type: 'f64', name: 'd' },
+    bigdecimal: { type: 'String', name: 'bigDecimal' },
+    byte: { type: 'u8', name: 'b' },
+    short: { type: 'i16', name: 's' },
+    long: { type: 'i64', name: 'l' },
+    biginteger: { type: 'String', name: 'bigInteger' },
+    '': { type: 'String', name: 'arg' },
   },
   defaultSnippetTemplate: `
-    @{{ keyword }}("{{ expression }}")
-    public void {{ #underscore }}{{ expression }}{{ /underscore }}({{ #parameters }}{{ #seenParameter }}, {{ /seenParameter }}{{ type }} {{ name }}{{ /parameters }}) {
+    #[{{ keyword }}(expr = "{{ expression }}")]
+    fn {{ #underscore }}{{ expression }}{{ /underscore }}(world: &mut TheWorld, {{ #parameters }}{{ #seenParameter }}, {{ /seenParameter }}{{ name }}: {{ type }}{{ /parameters }}) {
         // {{ blurb }}
     }
 `,
@@ -118,10 +104,11 @@ export const rustLanguage: Language = {
 
 function stringLiteral(node: TreeSitterSyntaxNode | null): string {
   if (node === null) throw new Error('node cannot be null')
+  if (node.text[0] === 'r') return unescapeString(node.text.slice(2, -1))
   return unescapeString(node.text.slice(1, -1))
 }
 
-// Java escapes \ as \\. Turn \\ back to \.
+// rust
 function unescapeString(s: string): string {
   return s.replace(/\\\\/g, '\\')
 }
