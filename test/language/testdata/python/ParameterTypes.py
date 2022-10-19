2eb8bdff6445fffc8e2c2f17585e38e20bf1ca9f
Dev Gupta <kaiserollofdoom@gmail.com>
2022-10-19 00:04:11 -0400

    WIP parameter type support

---
+++ test/language/testdata/python/ParameterTypes.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+ParameterType(
+    name="uuid",
+    regexp="/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/",
+    transformer=lambda x: x,
+)
+
+ParameterType(
+    name="date",
+    regexp="/\d{4}-\d{2}-\d{2}/",
+    transformer=lambda x: datetime.datetime(x),
+)
+
+ParameterType(
+    name="planet",
+    regexp="jupiter|mars|tellus",
+    transformer=lambda x: x,
+)
--- test/language/testdata/python/StepDefinitions.py
+++ test/language/testdata/python/StepDefinitions.py
@@ -4,7 +4,33 @@ import datetime
 import re
 
 
-@given("^a regexp$")
-def step_re(expression):
+@given("a {uuid}")
+def step_given(context, uuid):
+    assert uuid
+
+
+@given("a {date}")
+def step_date(context, date):
+    assert date
+
+
+@given("a {planet}")
+def step_planet(context, planet):
+    assert planet
+
+
+@given("an {undefined-parameter}")
+def step_undef(context, planet):
+    assert planet
+
+
+@given("/^a regexp$/")
+def step_re(context, expression):
+    """Test Re."""
+    assert expression
+
+
+@given("the bee's knees")
+def step_bees(context, expression):
     """Test Re."""
     assert expression
