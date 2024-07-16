9d651f50a82c3506fabe2b5be4eafe3a0c251fee
James Kent <jameskent91@gmail.com>
2024-07-16 15:45:48 +0100

    Adds other build suggestions to the index.ts file

--- src/suggestions/index.ts
+++ src/suggestions/index.ts
@@ -1,2 +1,4 @@
 export * from './buildSuggestions.js'
+export * from './buildSuggestionFromCucumberExpression.js'
+export * from './buildSuggestionsFromRegularExpression.js'
 export * from './types.js'
